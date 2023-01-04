40e984c8181770c94263064eee9fba2e4a1d82ee
Apoorva Sapra <a.sapra@decisioninc.com>
2023-01-04 16:10:19 +1100

    Updated login and signup link to firebase db and added forgot password

--- login/login_data.js
+++ login/login_data.js
@@ -1,3 +1,4 @@
+
 const firebaseConfig = {
     apiKey: "AIzaSyBoni0BSgzE8iAo2mhKVOiRt4LWS7cPbaQ",
     authDomain: "sourcign-2a97a.firebaseapp.com",
@@ -7,59 +8,160 @@ const firebaseConfig = {
     messagingSenderId: "829422727128",
     appId: "1:829422727128:web:537e95c6000459234b0cad",
     measurementId: "G-4NRHX5X53Q"
-  };
+};
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-
-
-var database  = firebase.database().ref('data');
-
-function UserRegister(){
-var email = document.getElementById('email').value;
-var password = document.getElementById('password').value;
-if (password.length<8){
-    document.getElementById("errorMsg").innerHTML = "Your password must include atleast 8 characters"
-    return false;
-    }
-if(!email && !password){
-    
-}
-firebase.auth().createUserWithEmailAndPassword(email,password).then(function(){
-    
-}).catch(function (error){
-    var errorcode = error.code;
-    var errormsg = error.message;
-});
-}
-
-//authentication
+const app = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
+var database = firebase.database();
 
-function SignIn(){
+
+// Initialize Firebase
+// const app = firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// Initialize Firebase Authentication and get a reference to the service
+// const auth = firebase.auth();
+
+// function UserRegister() {
+//     var email = document.getElementById('signup_email').value;
+//     var password = document.getElementById('signup_password').value;
+
+//     if (email && password) {
+//         if (password.length < 8) {
+//             document.getElementById("errorMsg").innerHTML = "Your password must include atleast 8 characters"
+//             return false;
+//         }
+//         auth.createUserWithEmailAndPassword(auth, email, password)
+//         .then((userCredential) => {
+//             // Signed in 
+//             const user = userCredential.user;
+//             db.set(db.ref('users/' + user.uid),{
+//                 name: name,
+//                 email: email
+//             });
+//         })
+//         .catch((error) => {
+//             const errorCode = error.code;
+//             const errorMessage = error.message;
+//             alert(errorMessage);
+            
+//         });
+//     }
+   
+// }
+
+//   signInWithEmailAndPassword(auth, email, password)
+//     .then((userCredential) => {
+//       // Signed in 
+//       const user = userCredential.user;
+//       // ...
+//     })
+//     .catch((error) => {
+//       const errorCode = error.code;
+//       const errorMessage = error.message;
+//       alert(errorMessage);
+//     });
+
+
+// function UserRegister(){
+// var email = document.getElementById('email').value;
+// var password = document.getElementById('password').value;
+// if (password.length<8){
+//     document.getElementById("errorMsg").innerHTML = "Your password must include atleast 8 characters"
+//     return false;
+//     }
+// if(!email && !password){
+
+// }
+// firebase.auth().createUserWithEmailAndPassword(email,password).then(function(){
+
+// }).catch(function (error){
+//     var errorcode = error.code;
+//     var errormsg = error.message;
+// });
+// }
+
+// authentication
+// const auth = firebase.auth();
+
+// function SignIn() {
+//     var email = document.getElementById('signin_email').value;
+//     var password = document.getElementById('signin_password').value;
+
+//     if(email && password){
+//         const promise = auth.signInWithEmailAndPassword(email, password);
+//         console.log("entered signin");
+//         promise.catch(e => alert(e.msg));
+//         window.open("../homepage_1.html", "_self");
+//     }
+
+// }
+
+document.getElementById('signin_form').addEventListener('submit', (e) => {
+    e.preventDefault();
     var email = document.getElementById('signin_email').value;
     var password = document.getElementById('signin_password').value;
 
-    const promise = auth.signInWithEmailAndPassword(email,password);
+    auth.signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        alert('User loged in!');
+        window.location.href= "../homepage_1.html";
 
-    promise.catch( e => alert(e.msg));
-    window.open("https://www.google.com","_self");
-}
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        alert(errorMessage);
+  });
+})
+
 
 document.getElementById('signup_form').addEventListener('submit', (e) => {
+
     e.preventDefault();
-    var userInfo = database.push();
+
+    var name = document.getElementById('signup_name').value;
+    var email = document.getElementById('signup_email').value;
+    var password = document.getElementById('signup_password').value;
+
+    if (name && email && password){
     
-    userInfo.set({
-        name: getId('name'),
-        email : getId('email'),
-        password : getId('password')
-    });
-    alert("Successfully Signed Up!");
-    console.log("data sent");
-    document.getElementById('form').reset();
+        if (password.length < 8) {
+            document.getElementById("errorMsg").innerHTML = "Your password must include atleast 8 characters"
+            return false;
+        }
+
+        auth.createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            const dt = new Date();
+            const user = userCredential.user;
+            firebase.database().ref('users/' + user.uid).set({
+                name: name,
+                email: email,
+              });
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage);
+            
+        });
+        
+        alert("Successfully Signed Up!");
+        console.log("data sent");
+        document.getElementById('signup_form').reset();
+        document.getElementById("errorMsg").innerHTML= "";
+    }
+    else{
+        alert("Please fill all the fields with * marked.")
+    }
+
 });
-function  getId(id){
+
+function getId(id) {
     return document.getElementById(id).value;
 }
